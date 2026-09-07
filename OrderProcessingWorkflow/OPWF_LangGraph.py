2b92e2127d16c1d1acece17e4892dce5d05e9c2d
aniketgore100 <goreaniket100@gmail.com>
2026-09-07 21:31:23 +0530

    Inventory node added

--- OrderProcessingWorkflow/OPWF_LangGraph.py
+++ OrderProcessingWorkflow/OPWF_LangGraph.py
@@ -48,19 +48,33 @@ def check_product(state: OrderState):
     ).fetchone()
 
     connection.close()
-    print("product :: ", product)
     if product is None:
         return {
             "order_status": "INVALID_PRODUCT"
         }
-
-    print("product ::", product["name"], product["price"])
-
     return {
         "order_status": "PRODUCT_VALID"
     }
 
 
+def check_inventry(state : OrderState):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    inventory = cursor.execute(
+        "select * from inventory where product_id = ?",
+        (state["product_id"],)
+    ).fetchone()
+
+    connection.close()
+
+    if inventory is None or inventory["quantity"] < state["quantity"]:
+        return{
+            "inventory_status" : "UNAVAILABLE"
+        }
+    return {
+        "inventory_status" : "AVAILABLE"
+    }
 
 
 def route_after_validation(state: OrderState) -> str:
@@ -69,10 +83,19 @@ def route_after_validation(state: OrderState) -> str:
     return "INVALID"
 
 
+def route_after_product_check(state : OrderState) -> str:
+    if state["order_status"] == "PRODUCT_VALID":
+        return "VALID"
+    return "INVALID"
 
+
+#Create State graph
 graph = StateGraph(OrderState)
+
+#Register Nodes here
 graph.add_node("validate_order", validate_order)
 graph.add_node("check_product", check_product)
+graph.add_node("check_inventory", check_inventry)
 
 
 graph.add_edge(START, "validate_order")
@@ -84,6 +107,15 @@ graph.add_conditional_edges(
         "INVALID" : END
     }
 )
-graph.add_edge("check_product", END)
+graph.add_conditional_edges(
+    "check_product",
+    route_after_product_check,
+    {
+        "VALID" : "check_inventory",
+        "INVALID" : END
+    }
+)
+
+graph.add_edge("check_inventory", END)
 
 app = graph.compile()
